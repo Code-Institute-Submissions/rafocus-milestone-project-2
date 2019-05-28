a7c650c634aa953fc97b6ff830cce8cc3a02632a
rafocus <rafikar@protonmail.com>
2019-05-28 19:51:17 +0100

    second pie chart

--- static/js/graph.js
+++ static/js/graph.js
@@ -9,11 +9,19 @@ function makeGraph(error, data) {
 
     //  define dimensions
     genreDimension = ndx.dimension(d=> d.genre)
+    oscarsDimension = ndx.dimension(d=> d.oscars)
     // define groups
     genreGroup = genreDimension.group()
+    oscarsGroup = oscarsDimension.group()
     // charts
 
-    let piechart1 = dc.pieChart("#chart")
+    let piechart1 = dc.pieChart("#piechart1")
+                    .width(400)
+                    .height(300)
+                    .dimension(genreDimension)
+                    .group(genreGroup)
+
+    let piechart2 = dc.pieChart("#piechart2")
                     .width(400)
                     .height(300)
                     .dimension(genreDimension)
